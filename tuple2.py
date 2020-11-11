535b697ed2bbe755f1faab633964c09825425795
mokkksh <73828893+mokkksh@users.noreply.github.com>
2020-11-11 18:02:01 +0530

    Add files via upload

---
+++ tuple2.py
@@ -0,0 +1,13 @@
+# acessing elements with tuple
+tuple1=('w','e','l','c','o','m','e')
+print(tuple1)
+print(tuple1[1])
+print(tuple1[3])
+print(tuple1[5])
+
+#nested tuple
+nest_tuple2 = ("point",[1,3,4],(8,7,9))
+print(nest_tuple2)
+print(nest_tuple2[0][3])
+print(nest_tuple2[1][2])
+print(nest_tuple2[2][2])
